4a8af6fed91af57388ef392c186f40c5fa42118d
roma_smm <roma.iswara@sinarmasmining.com>
2023-01-10 08:00:31 +0700

    [add]: add color scheme

--- lib/config/color_config.dart
+++ lib/config/color_config.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ColorConfig {
+  static Color primary = Colors.blue;
+  static Color secondary = Colors.pink;
+  static Color background = Colors.white;
+
   static Color white = Colors.white;
+  static Color black = Colors.black87;
+  static Color red = Colors.red;
+  static Color blue = Colors.blue;
+  static Color pink = Colors.pink;
+  static Color green = Colors.green;
+  static Color yellow = Colors.yellow;
 }
--- lib/config/theme_config.dart
+++ lib/config/theme_config.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'config.dart';
+
 class ThemeConfig {
   static ThemeData theme = ThemeData(
-    primarySwatch: Colors.blue,
+    colorScheme: ColorScheme(
+      brightness: Brightness.light,
+      primary: ColorConfig.primary,
+      onPrimary: ColorConfig.white,
+      secondary: ColorConfig.secondary,
+      onSecondary: ColorConfig.white,
+      error: ColorConfig.red,
+      onError: ColorConfig.white,
+      background: ColorConfig.background,
+      onBackground: ColorConfig.black,
+      surface: ColorConfig.green,
+      onSurface: ColorConfig.yellow,
+    ),
   );
 }
