74abdcc9d355b86a309076f48296bc138a6d7422
romaiswara <rhomajr27@gmail.com>
2023-01-17 22:05:44 +0700

    [add] dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,23 @@
+class DummyData {
+  static const String pathImages = 'assets/images';
+
+  static const List<String> photos = [
+    '$pathImages/photo_1.jpg',
+    '$pathImages/photo_2.jpg',
+    '$pathImages/photo_3.jpg',
+    '$pathImages/photo_4.jpg',
+    '$pathImages/photo_5.jpg',
+    '$pathImages/photo_6.jpg',
+    '$pathImages/photo_7.jpg',
+    '$pathImages/photo_8.jpg',
+    '$pathImages/photo_9.jpg',
+    '$pathImages/photo_10.jpg',
+    '$pathImages/photo_11.jpg',
+    '$pathImages/photo_12.jpg',
+    '$pathImages/photo_13.jpg',
+    '$pathImages/photo_14.jpg',
+    '$pathImages/photo_15.jpg',
+    '$pathImages/photo_16.jpg',
+    '$pathImages/photo_17.jpg',
+  ];
+}
