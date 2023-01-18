2003a2124d27b0c5c6f14b5979e2ef21b7e8a1ec
romaiswara <rhomajr27@gmail.com>
2023-01-18 21:21:45 +0700

    [fix] color config

--- lib/config/color_config.dart
+++ lib/config/color_config.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ColorConfig {
-  static const Color primary = Color(0xff7a5832);
-  static const Color secondary = Color(0xff1b8597);
+  static const Color primary = Color(0xffD2AA7E);
+  static const Color secondary = Color(0xff756649);
   static const Color white = Color(0xffffffff);
   static const Color black = Color(0xff000000);
   static const Color red = Color(0xffF32424);
   static const Color background = Color(0xfffaeddf);
-  static const Color brownLight = Color(0xfff0dfca);
+  static const Color brownLight = Color(0xffFCE7BB);
   static const Color blue = Color(0xff1b8597);
   static const Color orange = Color(0xfff8a677);
   static const Color yellow = Color(0xfffff48f);
