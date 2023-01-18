4367437f1df50871ae69a1b1aa604eba5c1fa487
roma_smm <roma.iswara@sinarmasmining.com>
2023-01-18 12:58:35 +0700

    [add]: adjust all UI bottom navigation

--- lib/ui/bottom_navigation/bottom_navigation_primary_screen.dart
+++ lib/ui/bottom_navigation/bottom_navigation_primary_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../config/config.dart';
 import '../../model/model.dart';
+import 'content_widget.dart';
 
 class BottomNavigationPrimaryScreen extends StatefulWidget {
   const BottomNavigationPrimaryScreen({Key? key}) : super(key: key);
@@ -15,11 +16,6 @@ class _BottomNavigationPrimaryScreenState
     extends State<BottomNavigationPrimaryScreen> {
   late int currentTab;
 
-  final List<String> _listProduct = [
-    'assets/images/product_5.png',
-    'assets/images/product_6.png',
-  ];
-
   @override
   void initState() {
     currentTab = 0;
@@ -29,86 +25,11 @@ class _BottomNavigationPrimaryScreenState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          /// header
-          SizedBox(
-            height: 113 + MediaQuery.of(context).padding.top,
-            child: Image.asset(
-              'assets/images/product_7.png',
-              fit: BoxFit.fill,
-            ),
-          ),
-          const SizedBox(height: 12),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'How to be Minimalist',
-                hintStyle: const TextStyle(
-                  fontSize: 12,
-                  color: Colors.black,
-                ),
-                fillColor: ColorConfig.primary,
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25),
-                  borderSide: BorderSide.none,
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 0,
-                ),
-                suffixIcon: const Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
-
-          /// body
-          Expanded(
-            child: ListView(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20,
-                horizontal: 20,
-              ),
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    Text('Aesthetic'),
-                    Text('White'),
-                    Text('Simper'),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                const Text('More', textAlign: TextAlign.end),
-                const SizedBox(height: 8),
-                Row(
-                  children: List.generate(
-                    _listProduct.length,
-                    (index) => Container(
-                      margin: EdgeInsets.only(left: index == 0 ? 0 : 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Image.asset(_listProduct[index]),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: ContentWidget(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: ThemeConfig.theme.primaryColor,
         elevation: 1,
-        child: Icon(
+        child: const Icon(
           Icons.add,
           color: ColorConfig.white,
         ),
@@ -130,8 +51,6 @@ class _BottomNavigationPrimaryScreenState
         },
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        selectedItemColor: ThemeConfig.theme.primaryColor,
-        unselectedItemColor: ThemeConfig.theme.primaryColor,
       ),
     );
   }
---
+++ lib/ui/bottom_navigation/content_widget.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/config/config.dart';
+import 'package:flutter_widgets/constant/constant.dart';
+import 'package:flutter_widgets/data/dummy_data.dart';
+
+class ContentWidget extends StatelessWidget {
+  ContentWidget({Key? key}) : super(key: key);
+
+  final List<String> newReleases = List.from(DummyData.photos)..shuffle();
+  final List<String> recommended = List.from(DummyData.photos)..shuffle();
+  final List<String> topRated = List.from(DummyData.photos)..shuffle();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Container(
+            height: kToolbarHeight,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Icon(Icons.arrow_back_ios_new),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      hintStyle: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                      ),
+                      fillColor: ColorConfig.primary,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 0,
+                      ),
+                      suffixIcon: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              children: [
+                const SizedBox(height: 20),
+
+                /// new release. show 2 image using horizontal list view
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'New Release',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'MORE',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                SizedBox(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(
+                      4,
+                      (index) => Container(
+                        margin: EdgeInsets.only(left: index == 0 ? 0 : 4),
+                        child: _photoWidget(photo: newReleases[index]),
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                /// card recommended. show 3 image using  horizontal list view
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Recommended',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'MORE',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(
+                      4,
+                      (index) => Container(
+                        margin: EdgeInsets.only(left: index == 0 ? 0 : 4),
+                        child: _photoWidget(photo: recommended[index]),
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                /// card top rated. show 4 image using  horizontal list view
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Top Rated',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'MORE',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(
+                      4,
+                      (index) => Container(
+                        margin: EdgeInsets.only(left: index == 0 ? 0 : 4),
+                        child: _photoWidget(photo: topRated[index]),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _photoWidget({required String photo}) {
+    return SizedBox(
+      width: 180,
+      child: Card(
+        elevation: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Image.asset(
+                photo,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          photo.split('/').last,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 4),
+                        const Text(
+                          StringConstant.shortLoremIpsum,
+                          style: TextStyle(fontSize: 12),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  const Icon(
+                    Icons.more_vert,
+                    color: Colors.black87,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
